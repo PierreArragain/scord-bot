25d18982d556f5b3f88a1f54ea9a8e31bd10a8f0
Pierre Arragain-Dehlinger <pierre@algorithma.fr>
2023-11-14 17:19:37 +0100

    V1

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import settings
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix="!", intents=intents)
+
+@client.event
+async def on_ready():
+    print("Scord-bot has logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    words = message.content.lower().split()
+
+    for word in words:
+        if word.startswith("di") or word.startswith("dy"):
+            if len(word) >= 4:
+                if word[2] in ("n", "m") and word[3] in ("a", "e", "i", "o", "u", "y"):
+                    response = word[2:]
+                else:
+                    return
+            else:
+                response = word[2:]
+
+            if response:
+                await message.channel.send(response)
+            break
+
+client.run(settings.TOKEN)
